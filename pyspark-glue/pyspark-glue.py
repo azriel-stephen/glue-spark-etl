dc7afac3d1571fccddaffdd52f2a858e51b63ac0
azriel-stephen <85186840+azriel-stephen@users.noreply.github.com>
2023-03-23 15:21:59 +0530

    Synchronizing job pyspark-glue from AWS Glue at 2023/03/23 09:51:59

--- pyspark-glue/pyspark-glue.py
+++ pyspark-glue/pyspark-glue.py
@@ -14,9 +14,28 @@ dyf = glueContext.create_dynamic_frame.from_catalog(database='pyspark_tutorial_d
 dyf.printSchema()
 df = dyf.toDF()
 df.show()
-dynamicFrameCustomers = dyf
-dynamicFrameCustomers.printSchema()
-dynamicFrameCustomers.show(10)
-dynamicFrameCustomers.count()
-dynamicFrameCustomers.select_fields(['customerid']).show(10)
+# s3output = glueContext.getSink(
+#   path="s3://bucket_name/folder_name",
+#   connection_type="s3",
+#   updateBehavior="UPDATE_IN_DATABASE",
+#   partitionKeys=[],
+#   compression="snappy",
+#   enableUpdateCatalog=True,
+#   transformation_ctx="s3output",
+# )
+# s3output.setCatalogInfo(
+#   catalogDatabase="demo", catalogTableName="populations"
+# )
+# s3output.setFormat("glueparquet")
+# s3output.writeFrame(DyF)
+dyf.printSchema()
+dyf.count()
+dyf.show(50)
+fullnames = dyf.select_fields(paths=['customerid', 'fullname'], transformation_ctx = 'get_names', info='get_names')
+fullnames.printSchema()
+fullnames.show()
+fullnames.toDF().show()
+only_full_names = dyf.drop_fields(paths = ['firstname', 'lastname'], transformation_ctx = 'drop_first_last', info = 'drop_first_last')
+only_full_names.toDF().show()
+only_full_names.count()
 job.commit()
